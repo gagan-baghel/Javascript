76372c11816b19aa204ea26806ffc7b49ed3fef5
Gagan baghel <78648366+gagan-baghel@users.noreply.github.com>
2023-02-04 23:16:47 +0530

    Delete script.js

--- Digitalclock/script.js
+++
@@ -1,9 +0,0 @@
-a = new Date();
-let date;
-let time;
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-setInterval(() => {
-	date = a.toLocaleDateString(undefined,options);
-	time = a.toLocaleTimeString('en-US');
-	document.getElementById("time").innerHTML =  time + " <br> on " + date;
-}, 1000);
