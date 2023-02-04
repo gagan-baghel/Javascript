6907fcb3f36124e958279acde471c87c549ad8e0
Gagan baghel <78648366+gagan-baghel@users.noreply.github.com>
2023-02-04 22:57:59 +0530

    Add files via upload

---
+++ CountdownClock/script.js
@@ -0,0 +1,31 @@
+const daysEle = document.getElementById('days');
+const hoursEle = document.getElementById('hours');
+const secondsEle = document.getElementById('seconds');
+const minutesEle = document.getElementById('minutes');
+
+const newyear = '1 Jan ' + (new Date().getFullYear()+1);
+console.log(newyear)
+
+
+
+function countdown(){
+	const newyeard = new Date(newyear)
+	const todayd = new Date()
+	const totalseconds= (newyeard-todayd)/1000;
+	const minutes = Math.floor(totalseconds/60 % 60) ;
+	const hours = Math.floor(totalseconds/3600)%24;
+	const days = Math.floor(totalseconds /3600/24) ;
+	const seconds = Math.floor(totalseconds % 60);
+
+	daysEle.innerHTML = formattime(days);
+	hoursEle.innerHTML = formattime(hours);
+	minutesEle.innerHTML = formattime(minutes);
+	secondsEle.innerHTML = formattime(seconds);
+
+}
+
+function formattime(time){
+	return time<10 ? `0${time}`: time;
+}
+countdown();
+setInterval(countdown, 1000);
